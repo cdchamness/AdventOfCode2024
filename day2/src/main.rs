8ca903ddce26bcf3466a09ece8d11ca3c28a4e97
cdchamness <113321462+cdchamness@users.noreply.github.com>
2024-12-02 21:39:32 -0500

    Refactored unnecessary Report struct

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -14,11 +14,11 @@ fn main() -> Result<()> {
     for line in input_line {
         match line {
             Ok(levels_string) => {
-                let report = Report::new(levels_string)?;
-                if Report::is_safe(&report.levels) {
+                let levels = parse_line(levels_string)?;
+                if is_safe(&levels) {
                     safe_count += 1;
                     dampener_safe_count += 1
-                } else if Report::is_safe_with_dampener(&report.levels) {
+                } else if is_safe_with_dampener(&levels) {
                     dampener_safe_count += 1;
                 }
             }
@@ -33,60 +33,54 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-struct Report {
-    levels: Vec<i64>,
+fn parse_line(levels_string: String) -> Result<Vec<i64>> {
+    let mut levels = Vec::new();
+    for str_val in levels_string.split_whitespace() {
+        if let Ok(val) = str_val.parse::<i64>() {
+            levels.push(val);
+        } else {
+            return Err(anyhow::Error::msg("Failed to parse str to u64"));
+        }
+    }
+    Ok(levels)
 }
 
-impl Report {
-    fn new(levels_string: String) -> Result<Report> {
-        let mut levels = Vec::new();
-        for str_val in levels_string.split_whitespace() {
-            if let Ok(val) = str_val.parse::<i64>() {
-                levels.push(val);
-            } else {
-                return Err(anyhow::Error::msg("Failed to parse str to u64"));
-            }
-        }
-        Ok(Report { levels })
+fn is_safe(levels: &[i64]) -> bool {
+    let mut diffs = Vec::new();
+    for i in 0..(levels.len() - 1) {
+        diffs.push(levels[i + 1] - levels[i]);
+    }
+    // Now that I have the differences between levels, I can just check for the fail cases and return early
+    // if none of the fail cases are satisfied it is safe
+    if diffs.contains(&0) {
+        // No change
+        return false;
+    }
+    if diffs.iter().min().is_some_and(|x| x.is_negative())
+        && diffs.iter().max().is_some_and(|x| x.is_positive())
+    {
+        // Increase and decrease
+        return false;
+    }
+    if !diffs
+        .into_iter()
+        .filter(|x| x.abs() > 3)
+        .collect::<Vec<i64>>()
+        .is_empty()
+    {
+        // There was at least 1 change that was larger than 3
+        return false;
+    }
+    true
+}
+
+fn is_safe_with_dampener(levels: &[i64]) -> bool {
+    let mut dampener_safe = Vec::new();
+    for i in 0..levels.len() {
+        let mut dampened_levels = levels.to_vec();
+        dampened_levels.remove(i);
+        dampener_safe.push(is_safe(&dampened_levels));
     }
 
-    fn is_safe(levels: &[i64]) -> bool {
-        let mut diffs = Vec::new();
-        for i in 0..(levels.len() - 1) {
-            diffs.push(levels[i + 1] - levels[i]);
-        }
-        // Now that I have the differences between levels, I can just check for the fail cases and return early
-        // if none of the fail cases are satisfied it is safe
-        if diffs.contains(&0) {
-            // No change
-            return false;
-        }
-        if diffs.iter().min().is_some_and(|x| x.is_negative())
-            && diffs.iter().max().is_some_and(|x| x.is_positive())
-        {
-            // Increase and decrease
-            return false;
-        }
-        if !diffs
-            .into_iter()
-            .filter(|x| x.abs() > 3)
-            .collect::<Vec<i64>>()
-            .is_empty()
-        {
-            // There was at least 1 change that was larger than 3
-            return false;
-        }
-        true
-    }
-
-    fn is_safe_with_dampener(levels: &[i64]) -> bool {
-        let mut dampener_safe = Vec::new();
-        for i in 0..levels.len() {
-            let mut dampened_levels = levels.to_vec();
-            dampened_levels.remove(i);
-            dampener_safe.push(Report::is_safe(&dampened_levels));
-        }
-
-        dampener_safe.contains(&true)
-    }
+    dampener_safe.contains(&true)
 }
